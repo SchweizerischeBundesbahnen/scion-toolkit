8fd97a87da06fac608fa6fdb5844a8d6c50cac74
Daniel Wiehl <daniel.wiehl@yahoo.com>
2020-06-06 21:06:08 +0200

    fix(toolkit): make sashbox component work with Angular 9

---
+++ projects/scion/toolkit/sashbox/src/sashbox-accessor.ts
@@ -0,0 +1,14 @@
+import { SciSashDirective } from './sash.directive';
+import { Observable } from 'rxjs';
+
+/**
+ * Allows accessing the sashbox component from sashes.
+ */
+export abstract class SciSashBoxAccessor {
+
+  public abstract get sashes(): SciSashDirective[];
+
+  public abstract get sashes$(): Observable<SciSashDirective[]>;
+
+  public abstract get direction(): 'column' | 'row';
+}
