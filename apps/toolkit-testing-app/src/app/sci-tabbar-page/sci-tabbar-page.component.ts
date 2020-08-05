d96652aadb16acd1b15de4766641e4a78d834d84
mofogasy <matthias.spicher@gmail.com>
2020-08-05 11:44:13 +0200

    feat(ɵtoolkit): allow organizing content in tabs
    closes #10

---
+++ apps/toolkit-testing-app/src/app/sci-tabbar-page/sci-tabbar-page.component.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018-2019 Swiss Federal Railways
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ */
+
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'sci-tabbar-page',
+  templateUrl: './sci-tabbar-page.component.html',
+  styleUrls: ['./sci-tabbar-page.component.scss'],
+})
+export class SciTabbarPageComponent {
+
+  public shortContentTabVisible = new FormControl(true);
+  public longContentTabVisible = new FormControl(true);
+  public textareaTabVisible = new FormControl(true);
+  public dynamicTabs = new FormControl('Tab 1,Tab 2,Tab 3');
+}
--- projects/scion/toolkit.internal/widgets/src/public_api.ts
+++ projects/scion/toolkit.internal/widgets/src/public_api.ts
@@ -20,3 +20,4 @@ export * from './form-field/public_api';
 export * from './params-enter/public_api';
 export * from './qualifier-chip-list/public_api';
 export * from './property/public_api';
+export * from './tabbar/public_api';
