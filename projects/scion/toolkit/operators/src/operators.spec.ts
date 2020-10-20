6476c33f84b7e0d2a870494088f687456d026ab2
danielwiehl <daniel.wiehl@yahoo.com>
2020-10-20 10:14:11 +0200

    feat(toolkit): allow running downstream and upstream operators inside a context

---
+++ projects/scion/toolkit/operators/src/operators.spec.ts
@@ -0,0 +1,349 @@
+/*
+ * Copyright (c) 2018-2019 Swiss Federal Railways
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ */
+
+import { noop, Observable, Observer, TeardownLogic } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+import { NgZone } from '@angular/core';
+import { finalize, tap } from 'rxjs/operators';
+import { observeInside, subscribeInside } from './operators';
+
+describe('Operators', () => {
+
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  describe('observeInside', () => {
+
+    it('should execute downstream, next operators and the next subscription handler inside the Angular zone', async () => {
+      const zone = TestBed.inject(NgZone);
+
+      // GIVEN
+      const insideAngularCaptor = {
+        onObservableCreate: undefined,
+        onObservableTeardown: undefined,
+        onNextBeforeObserveInside: undefined,
+        onNextAfterObserveInside: undefined,
+        onNext: undefined,
+        onFinalize: undefined,
+      };
+
+      const observable$ = new Observable((observer: Observer<void>): TeardownLogic => {
+        insideAngularCaptor.onObservableCreate = NgZone.isInAngularZone();
+        observer.next(); // emit an event
+        return () => insideAngularCaptor.onObservableTeardown = NgZone.isInAngularZone();
+      })
+        .pipe(
+          finalize(() => insideAngularCaptor.onFinalize = NgZone.isInAngularZone()),
+          tap(() => insideAngularCaptor.onNextBeforeObserveInside = NgZone.isInAngularZone()),
+          // use `observeInside` operator to continue operator chain inside the Angular zone
+          observeInside(continueFn => zone.run(continueFn)),
+          tap(() => insideAngularCaptor.onNextAfterObserveInside = NgZone.isInAngularZone()),
+        );
+
+      // WHEN
+      const subscription = observable$.subscribe(() => {
+        insideAngularCaptor.onNext = NgZone.isInAngularZone();
+      });
+
+      // THEN
+      await expect(NgZone.isInAngularZone()).toBeFalse();
+      await expect(insideAngularCaptor.onObservableCreate).toBeFalse();
+      await expect(insideAngularCaptor.onNextBeforeObserveInside).toBeFalse();
+      await expect(insideAngularCaptor.onNextAfterObserveInside).toBeTrue();
+      await expect(insideAngularCaptor.onNext).toBeTrue();
+
+      subscription.unsubscribe();
+
+      await expect(insideAngularCaptor.onObservableTeardown).toBeFalse();
+      await expect(insideAngularCaptor.onFinalize).toBeFalse();
+    });
+
+    it('should execute downstream complete operators and the complete subscription handler inside the Angular zone', async () => {
+      const zone = TestBed.inject(NgZone);
+
+      // GIVEN
+      const insideAngularCaptor = {
+        onObservableCreate: undefined,
+        onObservableTeardown: undefined,
+        onCompleteBeforeObserveInside: undefined,
+        onCompleteAfterObserveInside: undefined,
+        onComplete: undefined,
+        onFinalize: undefined,
+      };
+
+      const observable$ = new Observable((observer: Observer<void>): TeardownLogic => {
+        insideAngularCaptor.onObservableCreate = NgZone.isInAngularZone();
+        observer.complete(); // complete the Observable
+        return () => insideAngularCaptor.onObservableTeardown = NgZone.isInAngularZone();
+      })
+        .pipe(
+          finalize(() => insideAngularCaptor.onFinalize = NgZone.isInAngularZone()),
+          tap(noop, noop, () => insideAngularCaptor.onCompleteBeforeObserveInside = NgZone.isInAngularZone()),
+          // use `observeInside` operator to continue operator chain inside the Angular zone
+          observeInside(continueFn => zone.run(continueFn)),
+          tap(noop, noop, () => insideAngularCaptor.onCompleteAfterObserveInside = NgZone.isInAngularZone()),
+        );
+
+      // WHEN
+      const subscription = observable$.subscribe(noop, noop, () => {
+        insideAngularCaptor.onComplete = NgZone.isInAngularZone();
+      });
+
+      // THEN
+      await expect(NgZone.isInAngularZone()).toBeFalse();
+      await expect(insideAngularCaptor.onObservableCreate).toBeFalse();
+      await expect(insideAngularCaptor.onCompleteBeforeObserveInside).toBeFalse();
+      await expect(insideAngularCaptor.onCompleteAfterObserveInside).toBeTrue();
+      await expect(insideAngularCaptor.onComplete).toBeTrue();
+
+      subscription.unsubscribe();
+
+      await expect(insideAngularCaptor.onObservableTeardown).toBeFalse();
+      await expect(insideAngularCaptor.onFinalize).toBeFalse();
+    });
+
+    it('should execute downstream error operators and the error subscription handler inside the Angular zone', async () => {
+      const zone = TestBed.inject(NgZone);
+
+      // GIVEN
+      const insideAngularCaptor = {
+        onObservableCreate: undefined,
+        onObservableTeardown: undefined,
+        onErrorBeforeObserveInside: undefined,
+        onErrorAfterObserveInside: undefined,
+        onError: undefined,
+        onFinalize: undefined,
+      };
+
+      const observable$ = new Observable((observer: Observer<void>): TeardownLogic => {
+        insideAngularCaptor.onObservableCreate = NgZone.isInAngularZone();
+        observer.error('error'); // emit an error
+        return () => insideAngularCaptor.onObservableTeardown = NgZone.isInAngularZone();
+      })
+        .pipe(
+          finalize(() => insideAngularCaptor.onFinalize = NgZone.isInAngularZone()),
+          tap(noop, () => insideAngularCaptor.onErrorBeforeObserveInside = NgZone.isInAngularZone()),
+          // use `observeInside` operator to continue operator chain inside the Angular zone
+          observeInside(continueFn => zone.run(continueFn)),
+          tap(noop, () => insideAngularCaptor.onErrorAfterObserveInside = NgZone.isInAngularZone()),
+        );
+
+      // WHEN
+      const subscription = observable$.subscribe(noop, () => {
+        insideAngularCaptor.onError = NgZone.isInAngularZone();
+      });
+
+      // THEN
+      await expect(NgZone.isInAngularZone()).toBeFalse();
+      await expect(insideAngularCaptor.onObservableCreate).toBeFalse();
+      await expect(insideAngularCaptor.onErrorBeforeObserveInside).toBeFalse();
+      await expect(insideAngularCaptor.onErrorAfterObserveInside).toBeTrue();
+      await expect(insideAngularCaptor.onError).toBeTrue();
+
+      subscription.unsubscribe();
+
+      await expect(insideAngularCaptor.onObservableTeardown).toBeFalse();
+      await expect(insideAngularCaptor.onFinalize).toBeFalse();
+    });
+  });
+
+  describe('subscribeInside', () => {
+
+    it('should create the Observable and execute next operators and the next handler inside the Angular zone', async () => {
+      const zone = TestBed.inject(NgZone);
+
+      // GIVEN
+      const insideAngularCaptor = {
+        onObservableCreate: undefined,
+        onObservableTeardown: undefined,
+        onNextBeforeSubscribeInside: undefined,
+        onNextAfterSubscribeInside: undefined,
+        onNext: undefined,
+        onFinalize: undefined,
+      };
+
+      const observable$ = new Observable((observer: Observer<void>): TeardownLogic => {
+        insideAngularCaptor.onObservableCreate = NgZone.isInAngularZone();
+        observer.next(); // emit an event
+        return () => insideAngularCaptor.onObservableTeardown = NgZone.isInAngularZone();
+      })
+        .pipe(
+          finalize(() => insideAngularCaptor.onFinalize = NgZone.isInAngularZone()),
+          tap(() => insideAngularCaptor.onNextBeforeSubscribeInside = NgZone.isInAngularZone()),
+          // use `subscribeInside` operator to run downstream and upstream operators inside the Angular zone
+          subscribeInside(continueFn => zone.run(continueFn)),
+          tap(() => insideAngularCaptor.onNextAfterSubscribeInside = NgZone.isInAngularZone()),
+        );
+
+      // WHEN
+      const subscription = observable$.subscribe(() => {
+        insideAngularCaptor.onNext = NgZone.isInAngularZone();
+      });
+
+      // THEN
+      await expect(NgZone.isInAngularZone()).toBeFalse();
+      await expect(insideAngularCaptor.onObservableCreate).toBeTrue();
+      await expect(insideAngularCaptor.onNextBeforeSubscribeInside).toBeTrue();
+      await expect(insideAngularCaptor.onNextAfterSubscribeInside).toBeTrue();
+      await expect(insideAngularCaptor.onNext).toBeTrue();
+
+      subscription.unsubscribe();
+
+      await expect(insideAngularCaptor.onObservableTeardown).toBeTrue();
+      await expect(insideAngularCaptor.onFinalize).toBeTrue();
+    });
+
+    it('should create the Observable and execute error operators and the error handler inside the Angular zone', async () => {
+      const zone = TestBed.inject(NgZone);
+
+      // GIVEN
+      const insideAngularCaptor = {
+        onObservableCreate: undefined,
+        onObservableTeardown: undefined,
+        onErrorBeforeSubscribeInside: undefined,
+        onErrorAfterSubscribeInside: undefined,
+        onError: undefined,
+        onFinalize: undefined,
+      };
+
+      const observable$ = new Observable((observer: Observer<void>): TeardownLogic => {
+        insideAngularCaptor.onObservableCreate = NgZone.isInAngularZone();
+        observer.error('error'); // emit an error
+        return () => insideAngularCaptor.onObservableTeardown = NgZone.isInAngularZone();
+      })
+        .pipe(
+          finalize(() => insideAngularCaptor.onFinalize = NgZone.isInAngularZone()),
+          tap(noop, () => insideAngularCaptor.onErrorBeforeSubscribeInside = NgZone.isInAngularZone()),
+          // use `subscribeInside` operator to run downstream and upstream operators inside the Angular zone
+          subscribeInside(continueFn => zone.run(continueFn)),
+          tap(noop, () => insideAngularCaptor.onErrorAfterSubscribeInside = NgZone.isInAngularZone()),
+        );
+
+      // WHEN
+      const subscription = observable$.subscribe(noop, () => {
+        insideAngularCaptor.onError = NgZone.isInAngularZone();
+      });
+
+      // THEN
+      await expect(NgZone.isInAngularZone()).toBeFalse();
+      await expect(insideAngularCaptor.onObservableCreate).toBeTrue();
+      await expect(insideAngularCaptor.onErrorBeforeSubscribeInside).toBeTrue();
+      await expect(insideAngularCaptor.onErrorAfterSubscribeInside).toBeTrue();
+      await expect(insideAngularCaptor.onError).toBeTrue();
+
+      subscription.unsubscribe();
+
+      await expect(insideAngularCaptor.onObservableTeardown).toBeTrue();
+      await expect(insideAngularCaptor.onFinalize).toBeTrue();
+    });
+
+    it('should create the Observable and execute complete operators and the complete handler inside the Angular zone', async () => {
+      const zone = TestBed.inject(NgZone);
+
+      // GIVEN
+      const insideAngularCaptor = {
+        onObservableCreate: undefined,
+        onObservableTeardown: undefined,
+        onCompleteBeforeSubscribeInside: undefined,
+        onCompleteAfterSubscribeInside: undefined,
+        onComplete: undefined,
+        onFinalize: undefined,
+      };
+
+      const observable$ = new Observable((observer: Observer<void>): TeardownLogic => {
+        insideAngularCaptor.onObservableCreate = NgZone.isInAngularZone();
+        observer.complete(); // complete the Observable
+        return () => insideAngularCaptor.onObservableTeardown = NgZone.isInAngularZone();
+      })
+        .pipe(
+          finalize(() => insideAngularCaptor.onFinalize = NgZone.isInAngularZone()),
+          tap(noop, noop, () => insideAngularCaptor.onCompleteBeforeSubscribeInside = NgZone.isInAngularZone()),
+          // use `subscribeInside` operator to run downstream and upstream operators inside the Angular zone
+          subscribeInside(continueFn => zone.run(continueFn)),
+          tap(noop, noop, () => insideAngularCaptor.onCompleteAfterSubscribeInside = NgZone.isInAngularZone()),
+        );
+
+      // WHEN
+      const subscription = observable$.subscribe(noop, noop, () => {
+        insideAngularCaptor.onComplete = NgZone.isInAngularZone();
+      });
+
+      // THEN
+      await expect(NgZone.isInAngularZone()).toBeFalse();
+      await expect(insideAngularCaptor.onObservableCreate).toBeTrue();
+      await expect(insideAngularCaptor.onCompleteBeforeSubscribeInside).toBeTrue();
+      await expect(insideAngularCaptor.onCompleteAfterSubscribeInside).toBeTrue();
+      await expect(insideAngularCaptor.onComplete).toBeTrue();
+
+      subscription.unsubscribe();
+
+      await expect(insideAngularCaptor.onObservableTeardown).toBeTrue();
+      await expect(insideAngularCaptor.onFinalize).toBeTrue();
+    });
+  });
+
+  describe('subscribeInside and observeInside', () => {
+
+    it('should subscribe outside the Angular zone, but observe inside of the Angular zone ', async () => {
+      const zone = TestBed.inject(NgZone);
+
+      // GIVEN
+      const insideAngularCaptor = {
+        onObservableCreate: undefined,
+        onObservableTeardown: undefined,
+        onNextBeforeSubscribeInside: undefined,
+        onNextAfterSubscribeInside: undefined,
+        onNextAfterObserveInside: undefined,
+        onNext: undefined,
+        onComplete: undefined,
+        onFinalize: undefined,
+      };
+
+      const observable$ = new Observable((observer: Observer<void>): TeardownLogic => {
+        insideAngularCaptor.onObservableCreate = NgZone.isInAngularZone();
+        observer.next(); // emit an event
+        observer.complete();
+        return () => insideAngularCaptor.onObservableTeardown = NgZone.isInAngularZone();
+      })
+        .pipe(
+          finalize(() => insideAngularCaptor.onFinalize = NgZone.isInAngularZone()),
+          tap(() => insideAngularCaptor.onNextBeforeSubscribeInside = NgZone.isInAngularZone()),
+          // use `subscribeInside` operator to run downstream and upstream operators inside the Angular zone
+          subscribeInside(continueFn => zone.runOutsideAngular(continueFn)),
+          tap(() => insideAngularCaptor.onNextAfterSubscribeInside = NgZone.isInAngularZone()),
+          // use `observeInside` operator to run downstream operators inside the Angular zone
+          observeInside(continueFn => zone.run(continueFn)),
+          tap(() => insideAngularCaptor.onNextAfterObserveInside = NgZone.isInAngularZone()),
+        );
+
+      // WHEN
+      await zone.run(async () => {
+        const subscription = observable$.subscribe(
+          () => insideAngularCaptor.onNext = NgZone.isInAngularZone(),
+          noop,
+          () => insideAngularCaptor.onComplete = NgZone.isInAngularZone(),
+        );
+
+        // THEN
+        await expect(NgZone.isInAngularZone()).toBeTrue();
+        await expect(insideAngularCaptor.onObservableCreate).toBeFalse();
+        await expect(insideAngularCaptor.onNextBeforeSubscribeInside).toBeFalse();
+        await expect(insideAngularCaptor.onNextAfterSubscribeInside).toBeFalse();
+        await expect(insideAngularCaptor.onNextAfterObserveInside).toBeTrue();
+        await expect(insideAngularCaptor.onNext).toBeTrue();
+        await expect(insideAngularCaptor.onComplete).toBeTrue();
+
+        subscription.unsubscribe();
+
+        await expect(insideAngularCaptor.onObservableTeardown).toBeFalse();
+        await expect(insideAngularCaptor.onFinalize).toBeFalse();
+      });
+    });
+  });
+});
--- projects/scion/toolkit/operators/src/operators.ts
+++ projects/scion/toolkit/operators/src/operators.ts
@@ -1,5 +1,15 @@
+/*
+ * Copyright (c) 2018-2019 Swiss Federal Railways
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ */
+
 import { map } from 'rxjs/operators';
-import { MonoTypeOperatorFunction, OperatorFunction, SchedulerLike } from 'rxjs';
+import { MonoTypeOperatorFunction, noop, Observable, Observer, OperatorFunction, SchedulerLike, Subscriber, Subscription, TeardownLogic } from 'rxjs';
 
 /**
  * Filters items in the source array and emits an array with items satisfying given predicate.
@@ -37,3 +47,130 @@ export function tapFirst<T>(tapFn: (value: T) => void, scheduler?: SchedulerLike
     return value;
   }));
 }
+
+/**
+ * Mirrors the source Observable, but runs downstream operators (operators below the `observeInside` operator) and subscription handlers
+ * (next, error, complete) inside the given execution context.
+ *
+ * This operator is particularly useful in Angular applications to run downstream operators inside or outside the Angular zone,
+ * as following: `observeInside(continueFn => ngzone.run(continueFn))`.
+ *
+ * This operator is similar to RxJS's `observeOn` operator, but instead of a scheduler, it accepts an executor. An executor is a function to
+ * create an execution context in which downstream operators are then executed. The function is called with a single argument, a function
+ * to continue the execution chain.
+ *
+ * #### Example showing in which Angular zone operators run:
+ * ```
+ * // Code running outside Angular
+ *
+ * interval(1000) // Observable creation outside Angular
+ *   .pipe(
+ *     tap(() => ...), // outside Angular
+ *     tap(() => ...), // outside Angular
+ *     observeInside(continueFn => zone.run(continueFn)),
+ *     tap(() => ...), // inside Angular
+ *   )
+ *   .subscribe(); // inside Angular
+ * ```
+ *
+ * @param   executionFn - Function for setting up a context in which to continue the execution chain.
+ * @return  An Observable mirroring the source, but running downstream operators in a context.
+ */
+export function observeInside<T>(executionFn: ExecutionFn): MonoTypeOperatorFunction<T> {
+  return (source: Observable<T>): Observable<T> => {
+    return new Observable((observer: Observer<T>): TeardownLogic => {
+      const subscription = source.subscribe(
+        next => executionFn(() => observer.next(next)),
+        error => executionFn(() => observer.error(error)),
+        () => executionFn(() => observer.complete()),
+      );
+
+      return () => subscription.unsubscribe();
+    });
+  };
+}
+
+/**
+ * Mirrors the source Observable, but uses the given execution context to subscribe/unsubscribe to the source. It further runs all operators of the
+ * execution chain (operators above and below the `subscribeInside` operator) as well as subscription handlers (next, error, complete) in the given
+ * context.
+ *
+ * Unlike `observeInside` operator, the `subscribeInside` also acts upstream. By using the {@link observeInside} operator after the
+ * {@link subscribeInside}, you can change the execution context for downstream operators.
+ *
+ * This operator is particularly useful in Angular applications to subscribe to the source inside or outside the Angular zone, as following:
+ * `subscribeInside(continueFn => ngzone.run(continueFn))`.
+ *
+ * This operator is similar to RxJS's `subscribeOn` operator, but instead of a scheduler, it accepts an executor. An executor is a function to
+ * create an execution context in which upstream and downstream operators are then executed. The function is called with a single argument,
+ * a function to continue the execution chain.
+ *
+ * #### Example showing in which Angular zone operators run:
+ * ```
+ * // Code running outside Angular
+ *
+ * interval(1000) // Observable creation inside Angular
+ *   .pipe(
+ *     tap(() => ...), // inside Angular
+ *     tap(() => ...), // inside Angular
+ *     subscribeInside(continueFn => zone.run(continueFn)),
+ *     tap(() => ...), // inside Angular
+ *   )
+ *   .subscribe(); // inside Angular
+ * ```
+ *
+ * @param   executionFn - Function for setting up a context in which to continue the execution chain.
+ * @return  An Observable mirroring the source, but running upstream and downstream operators in a context.
+ */
+export function subscribeInside<T>(executionFn: ExecutionFn): MonoTypeOperatorFunction<T> {
+  return (source: Observable<T>): Observable<T> => {
+    return new Observable((observer: Observer<T>): TeardownLogic => {
+      let subscription: Subscription;
+
+      executionFn(() => {
+        subscription = source.subscribe(new class extends Subscriber<T> { // tslint:disable-line:new-parens
+
+          constructor() {
+            super(observer);
+          }
+
+          protected _next(value: T): void {
+            executionFn(() => super._next(value));
+          }
+
+          protected _error(err: any): void {
+            executionFn(() => super._error(err));
+          }
+
+          protected _complete(): void {
+            executionFn(() => super._complete());
+          }
+
+          public unsubscribe(): void {
+            executionFn(() => this.closed ? noop() : super.unsubscribe());
+          }
+        });
+      });
+
+      return () => {
+        executionFn(() => subscription?.unsubscribe());
+      };
+    });
+  };
+}
+
+/**
+ * Function to continue the operator chain.
+ *
+ * @see observeInside
+ * @see subscribeInside
+ */
+export type ContinueExecutionFn = () => void;
+
+/**
+ * Function for setting up a context in which to continue the execution chain.
+ *
+ * @see observeInside
+ * @see subscribeInside
+ */
+export type ExecutionFn = (fn: ContinueExecutionFn) => void;
