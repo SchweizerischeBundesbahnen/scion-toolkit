0d571462838641b42650081393339d33797cd9c3
danielwiehl <daniel.wiehl@yahoo.com>
2023-06-15 16:23:09 +0200

    refactor(component-app): move application config to separate file

--- apps/components/src/main.ts
+++ apps/components/src/main.ts
@@ -9,13 +9,6 @@
  */
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
-import {provideRouter, withHashLocation} from '@angular/router';
-import {provideAnimations} from '@angular/platform-browser/animations';
-import {routes} from './app/app.routes';
+import {appConfig} from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes, withHashLocation()),
-    provideAnimations(),
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
