8b742063387011cc4c678b90cc5ba1605961a1d2
Marcarrian <marc.muster@bluewin.ch>
2024-10-30 09:51:04 +0100

    test: start applications with Vercel `serve` instead of `http-server`
    The `http-server` package is no longer maintained and does not support custom headers.

--- projects/scion/components.e2e/playwright.config.ts
+++ projects/scion/components.e2e/playwright.config.ts
@@ -16,7 +16,7 @@ const runHeadless = !!process.env['HEADLESS'];
 const config: PlaywrightTestConfig = {
   forbidOnly: runInCI,
   webServer: {
-    command: runInCI ? 'npm run components-testing-app:http-server' : 'npm run components-testing-app:serve',
+    command: runInCI ? 'npm run components-testing-app:dist-serve' : 'npm run components-testing-app:serve',
     port: 4200,
     reuseExistingServer: !runInCI,
   },
