cdf60a079e3234a333ba8c316a2d9cfed2b7720f
danielwiehl <daniel.wiehl@yahoo.com>
2025-05-29 11:39:31 +0200

    feat(components/throbber): remove deprecated `SciThrobberModule`
    BREAKING CHANGE: Removed deprecated `SciThrobberModule`. Import the standalone `SciThrobberComponent` instead.

--- projects/scion/components/throbber/src/public_api.ts
+++ projects/scion/components/throbber/src/public_api.ts
@@ -13,5 +13,4 @@
  *
  * @see https://github.com/ng-packagr/ng-packagr/blob/master/docs/secondary-entrypoints.md
  */
-export {SciThrobberModule} from './throbber.module';
 export {SciThrobberComponent} from './throbber.component';
