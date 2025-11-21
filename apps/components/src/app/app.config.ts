be8c19fccec48de7b0b70fa981057a0f03979868
Marcarrian <marc.muster@bluewin.ch>
2025-11-21 12:01:03 +0100

    feat(components): add support for Angular 21
    BREAKING CHANGE: SCION Components requires Angular 21.
    
    Note that:
    - SCION Components does not support zoneless mode and may not work as expected. Support is planned for 2026.
    - SCION Components still requires `@angular/animations`. Removal is planned for 2026.

--- apps/components/src/app/app.config.ts
+++ apps/components/src/app/app.config.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2023 Swiss Federal Railways
+ * Copyright (c) 2018-2025 Swiss Federal Railways
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -8,7 +8,7 @@
  * SPDX-License-Identifier: EPL-2.0
  */
 
-import {ApplicationConfig} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter, withHashLocation} from '@angular/router';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {routes} from './app.routes';
@@ -20,5 +20,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withHashLocation()),
     provideAnimations(),
+    provideZoneChangeDetection(),
   ],
 };
