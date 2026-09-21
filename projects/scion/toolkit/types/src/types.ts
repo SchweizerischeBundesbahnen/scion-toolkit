19d3015670f9ac6ee3f1ca3cdb61ec5c59424fdc
danielwiehl <daniel.wiehl@yahoo.com>
2026-09-21 07:13:13 +0200

    feat(toolkit/types): provide @scion/toolkit/types
    This secondary entry point provides framework-agnostic types to `@scion/components` and `@scion/workbench`.

---
+++ projects/scion/toolkit/types/src/types.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018-2026 Swiss Federal Railways
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+import {Observable} from 'rxjs';
+
+/**
+ * Requires at least one key from T.
+ */
+export type RequireOne<T> = {
+  [K in keyof T]: Required<Pick<T, K>> & Partial<Omit<T, K>>
+}[keyof T];
+
+/**
+ * Allows maximum one property of T.
+ */
+export type OneOf<T> = {
+  [K in keyof T]: {[P in K]: T[P]} & {[P in Exclude<keyof T, K>]?: never};
+}[keyof T];
+
+/**
+ * Represents a value or an array of that value.
+ */
+export type MaybeArray<T> = T | T[];
+
+/**
+ * Represents a value or an {@link Observable} of that value.
+ */
+export type MaybeObservable<T> = T | Observable<T>;
+
+/**
+ * Represents an object that can be disposed to free resources.
+ */
+export interface Disposable {
+  /**
+   * Disposes the object, releasing any allocated resources.
+   */
+  dispose(): void;
+}
+
+/**
+ * Signature of a function to free allocated resources.
+ */
+export type DisposeFn = () => void;
